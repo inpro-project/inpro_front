ec23c7269310144dad7065991f8e662a28665bde
hainho <hainho9704@gmail.com>
2022-12-12 13:26:38 +0900

    🐛 bugfix : access token env에서 읽어오던거 쿠키에서 읽어오도록 수정

--- src/views/Teaminfo/UserLikeTeamInfoView.vue
+++ src/views/Teaminfo/UserLikeTeamInfoView.vue
@@ -50,7 +50,7 @@ export default {
       const allowuser = { teamIdx: teamIdx, userIdx: userLikeIdx, role: this.role }
       console.log(allowuser)
       await axios
-        .post(process.env.VUE_APP_API_BASE_URL + '/app/members', allowuser, { headers: { 'Content-Type': 'application/json', Authorization: process.env.VUE_APP_ACCESS_TOKEN } })
+        .post(process.env.VUE_APP_API_BASE_URL + '/app/members', allowuser, { headers: { 'Content-Type': 'application/json', Authorization: VueCookies.get('Authorization') } })
         .then(res => {
           console.log(res)
         })
@@ -62,7 +62,7 @@ export default {
     async passLike () { // 유저 좋아요 넘기기
       const userpassIdx = this.$route.params.userIdx
       await axios
-        .post(process.env.VUE_APP_API_BASE_URL + '/app/user-passes/' + userpassIdx, userpassIdx, { headers: { 'Content-Type': 'application/json', Authorization: process.env.VUE_APP_ACCESS_TOKEN } })
+        .post(process.env.VUE_APP_API_BASE_URL + '/app/user-passes/' + userpassIdx, userpassIdx, { headers: { 'Content-Type': 'application/json', Authorization: VueCookies.get('Authorization') } })
         .then(res => {
           console.log(res)
         })
